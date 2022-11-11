65d4f38a29e1cc29cbb5c27384652a48cd28894b
YOGESH SINGH YADAV <86778735+YOGDEV@users.noreply.github.com>
2022-11-11 23:01:09 +0530

    Create Primes sum.cpp

---
+++ Primes sum.cpp
@@ -0,0 +1,47 @@
+//https://practice.geeksforgeeks.org/problems/primes-sum5827/1
+
+//code
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+    bool prime(int n)
+    {
+        for(int i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0)return false;
+        }
+        return true;
+    }
+    string isSumOfTwo(int N){
+        
+        for(int i=2;i<=N/2;i++)
+        {
+            if(prime(i)&& prime(N-i))
+            return "Yes";
+        }
+        return "No";
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isSumOfTwo(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
