a14e9d54a95a0f12ad24dcbb6704a0dd5a0e866a
YOGESH SINGH YADAV <86778735+YOGDEV@users.noreply.github.com>
2022-11-17 20:55:43 +0530

    Create 223. Rectangle Area.cpp

---
+++ 223. Rectangle Area.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/rectangle-area/
+
+//code
+
+int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int total_area=0,sub;
+        
+        int area1=(ax2-ax1)*(ay2-ay1);
+        int area2=(bx2-bx1)*(by2-by1);
+        
+        total_area=area1+area2;
+        if (bx1 >= ax2 || ax1 >= bx2 || by1 >= ay2 || ay1 >= by2)return total_area;
+        cout<<total_area;
+        
+        sub=(min(ax2,bx2)-max(ax1,bx1))*(min(ay2,by2)-max(ay1,by1));
+        total_area-=sub;
+        return total_area;
+    }
