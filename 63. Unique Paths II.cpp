cb2a7ad6161bc5bc548d08f6f7b110fccc499e84
YOGESH SINGH YADAV <86778735+YOGDEV@users.noreply.github.com>
2023-02-27 01:04:30 +0530

    Create 63. Unique Paths II.cpp

---
+++ 63. Unique Paths II.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/unique-paths-ii/
+
+//code
+
+//tabulation 
+//TC => (nxm)
+//Sc => (nxm) with no stack space...
+
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                else if(i==0 and j==0)dp[i][j]=1;
+                else
+                {
+                    int up=0;
+                    int left=0;
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0)left=dp[i][j-1];
+                    
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
